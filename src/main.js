284125feac8f07ed358efa890f4952e4d48a6787
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-12-15 16:40:47 +0700

    Adding admin-lte bootstrap etc.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/style.scss";
+import "./bootstrap";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  // eslint-disable-next-line prettier/prettier
+  render: (h) => h(App),
 }).$mount("#app");
